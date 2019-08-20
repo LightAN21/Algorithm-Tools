358d828bd02812c87a97827e4d670dc0518d3ee2
Jonathan Tsai <jtsai@e1z3r5p18.42.us.org>
2019-08-20 10:44:40 -0700

    add

---
+++ priority_queue/min_priority_queue.js
@@ -0,0 +1,45 @@
+function new_min_priority_queue() {
+    var q = {
+        front: null,
+        rear: null,
+        length: 0,
+        push: min_priority_queue_push,
+        pop: min_priority_queue_pop,
+        pop_min: min_priority_queue_pop_min,
+        get_min: min_priority_queue_get_min,
+        get_front: min_priority_queue_get_front,
+        get_rear: min_priority_queue_get_rear,
+        print: min_priority_queue_print,
+    };
+    return q;
+}
+
+function min_priority_queue_push()
+{
+
+}
+function min_priority_queue_pop()
+{
+
+}
+function min_priority_queue_pop_min()
+{
+
+}
+function min_priority_queue_get_min()
+{
+
+}
+function min_priority_queue_get_front()
+{
+
+}
+function min_priority_queue_get_rear()
+{
+
+}
+function min_priority_queue_print()
+{
+
+}
+
