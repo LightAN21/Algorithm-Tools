2a1fcadd06dd240002c670e26c766c87dbc89776
Jonathan Tsai <jtsai@e1z3r7p13.42.us.org>
2019-08-29 10:26:26 -0700

    add

--- priority_queue/double_linked_list_queue.js
+++ priority_queue/double_linked_list_queue.js
@@ -1,12 +1,16 @@
-function new_double_linked_list_queue() {
+function new_doubly_linked_list_queue() {
     return {
         first: null,
         last: null,
         length: 0,
+        push: doubly_linked_list_queue_push,
+        pop: doubly_linked_list_queue_pop,
+        remove: doubly_linked_list_remove_node,
+        connect: doubly_linked_list_connect_nodes,
     };
 }
 
-function new_double_linked_list_queue_node(data) {
+function new_doubly_linked_list_node(data) {
     return {
         data: data,
         pre: null,
@@ -14,8 +18,8 @@ function new_double_linked_list_queue_node(data) {
     };
 }
 
-function double_linked_list_queue_push(data) {
-    var n = new_double_linked_list_queue_node(data);
+function doubly_linked_list_queue_push(data) {
+    var n = new_doubly_linked_list_node(data);
 
     if (this.first != null) {
         n.pre = this.last;
@@ -29,14 +33,31 @@ function double_linked_list_queue_push(data) {
         this.first = n;
         this.last = n;
     }
+    this.length++;
 }
 
-function double_linked_list_queue_pop() {
-    var pop_data = this.first.data;
+function doubly_linked_list_queue_pop() {
+    if (this.first == null)
+        return null;
 
+    var pop_data = this.first.data;
     this.first.next.pre = this.last;
     this.last.next = this.first.next;
     this.first = this.first.next;
+    this.length--;
     return pop_data;
 }
 
+function doubly_linked_list_remove_node(n) {
+    if (n == null)
+        return null
+    n.pre.next = n.next;
+    n.next.pre = n.pre;
+    this.length--;
+    return n;
+}
+
+function doubly_linked_list_connect_nodes(n, m) {
+    n.next = m;
+    m.pre = n;
+}
--- priority_queue/min_priority_queue.js
+++ priority_queue/min_priority_queue.js
@@ -1,144 +1,196 @@
-// Fibonacci heap
+// // Fibonacci heap
 
 function new_min_priority_queue() {
-    var q = {
+    return {
         min: null,
-        first: null,
-        last: null,
+        root_list: new_doubly_linked_list_queue(),
         length: 0,
         push: min_priority_queue_push,
         pop_min: min_priority_queue_pop_min,
         get_min: min_priority_queue_get_min_data,
         print: min_priority_queue_prnit,
     };
-    return q;
 }
 
 function new_priority_queue_node(data, priority) {
     return {
         data: data,
         priority: priority,
-        pre: null,
-        next: null,
-        children_first: null,
-        children_last: null,
-        rank: 0,
+        children_list: new_doubly_linked_list_queue(),
     };
 }
 
-function min_priority_queue_push(data, priority) {
+function min_priority_queue_push(data, priority = 0) {
     var n = new_priority_queue_node(data, priority);
-
-    if (this.min == null) {
-        n.pre = n;
-        n.next = n;
+    this.root_list.push(n);
+    if (this.min == null)
         this.min = n;
-        this.first = n;
-        this.last = n;
-    }
-    else {
-        n.next = this.first;
-        n.pre = this.last;
-        this.last.next = n;
-        this.first.pre = n;
-        this.last = n;
-        if (priority < this.min.priority)
-            this.min = n;
-    }
+    else if (n.priority < this.min.priority)
+        this.min = n;
+    this.length++;
 }
+
 function min_priority_queue_pop_min() {
     var m = this.min, n, mark = 0;
     var num = {};
 
     if (m == null)
         return null;
-    melt_children_to_root_list(m);
-    for (n = this.first; n != this.first || mark != 1; n = n.next) {
-
-        if (n.priority < this.min.priority) {
-            this.min = n;
-        }
-
-        var pre = num[n.rank]
-        if (pre == undefined) {
-            pre = n;
-        }
-        else if (pre.priority <= n.priority) {
-            add_tree_to_children(n, pre);
-
-        }
-        else {
-            add_tree_to_children(pre, n);
-        }
-        if (mark == 0)
-            mark = 1;
-    }
-    return m;
-}
-
-function melt_children_to_root_list(m) {
-    if (m == null)
-        return;
-    if (m.children_first == null) {
-        m.pre.next = m.next;
-        m.next.pre = m.pre;
-    }
-    else {
-        m.pre.next = m.children_first;
-        m.children_first.pre = m.pre;
-        m.children_last.next = m.next;
-        m.next.pre = m.children_last;
-    }
+    melt_children_to_root_list(m, this.root_list);
     this.min = this.first;
 }
 
-// add tree n to children of m.
-function add_tree_to_children(n, m) {
-    if (m.children_first == null) {
-        m.children_first = n;
-        m.children_last = n;
-        n.pre = n;
-        n.next = n;
+function melt_children_to_root_list(m, root_list) {
+    if (m == null)
+        return;
+    if (m.children_list.first == null) {
+        root_list.connect(m.pre, m.next);
     }
     else {
-        n.next = m.children_first;
-        n.pre = m.children_last;
-        m.children_last.next = n;
-        m.children_first.pre = n;
-        m.children_last = n;
+        root_list.connect(m.pre, m.children_list.first);
+        root_list.connect(m.children_list.last, m.next);
     }
 }
 
-function min_priority_queue_get_min_data() {
-    return this.min.data;
-}
+// function new_min_priority_queue() {
+//     var q = {
+//         min: null,
+//         first: null,
+//         last: null,
+//         length: 0,
+//         push: min_priority_queue_push,
+//         pop_min: min_priority_queue_pop_min,
+//         get_min: min_priority_queue_get_min_data,
+//         print: min_priority_queue_prnit,
+//     };
+//     return q;
+// }
 
-function min_priority_queue_prnit() {
-    var n = this.first;
+// function new_priority_queue_node(data, priority) {
+//     return {
+//         data: data,
+//         priority: priority,
+//         pre: null,
+//         next: null,
+//         children_first: null,
+//         children_last: null,
+//         rank: 0,
+//     };
+// }
 
-    console.log('');
-    console.log(n);
-    for (n = n.next; n != null && n != this.first; n = n.next) {
-        console.log(n);
-    }
-    console.log('');
-    console.log('min:');
-    console.log(this.min);
-    console.log('================================================================================');
-}
+// function min_priority_queue_push(data, priority) {
+//     var n = new_priority_queue_node(data, priority);
 
-var pq = new_min_priority_queue();
+//     if (this.min == null) {
+//         n.pre = n;
+//         n.next = n;
+//         this.min = n;
+//         this.first = n;
+//         this.last = n;
+//     }
+//     else {
+//         n.next = this.first;
+//         n.pre = this.last;
+//         this.last.next = n;
+//         this.first.pre = n;
+//         this.last = n;
+//         if (priority < this.min.priority)
+//             this.min = n;
+//     }
+// }
+// function min_priority_queue_pop_min() {
+//     var m = this.min, n, mark = 0;
+//     var num = {};
 
-pq.push('2_1', 2);
-pq.push('6', 6);
-pq.push('1', 1);
-pq.push('2_2', 2);
-pq.push('7', 7);
-pq.push('42', 42);
-pq.push('26', 26);
-pq.push('3', 3);
-pq.push('52', 52);
-pq.print();
+//     if (m == null)
+//         return null;
+//     melt_children_to_root_list(m);
+//     for (n = this.first; n != this.first || mark != 1; n = n.next) {
 
-pq.pop_min();
-pq.print();
+//         if (n.priority < this.min.priority) {
+//             this.min = n;
+//         }
+
+//         var pre = num[n.rank]
+//         if (pre == undefined) {
+//             pre = n;
+//         }
+//         else if (pre.priority <= n.priority) {
+//             add_tree_to_children(n, pre);
+
+//         }
+//         else {
+//             add_tree_to_children(pre, n);
+//         }
+//         if (mark == 0)
+//             mark = 1;
+//     }
+//     return m;
+// }
+
+// function melt_children_to_root_list(m) {
+//     if (m == null)
+//         return;
+//     if (m.children_first == null) {
+//         m.pre.next = m.next;
+//         m.next.pre = m.pre;
+//     }
+//     else {
+//         m.pre.next = m.children_first;
+//         m.children_first.pre = m.pre;
+//         m.children_last.next = m.next;
+//         m.next.pre = m.children_last;
+//     }
+//     this.min = this.first;
+// }
+
+// // add tree n to children of m.
+// function add_tree_to_children(n, m) {
+//     if (m.children_first == null) {
+//         m.children_first = n;
+//         m.children_last = n;
+//         n.pre = n;
+//         n.next = n;
+//     }
+//     else {
+//         n.next = m.children_first;
+//         n.pre = m.children_last;
+//         m.children_last.next = n;
+//         m.children_first.pre = n;
+//         m.children_last = n;
+//     }
+// }
+
+// function min_priority_queue_get_min_data() {
+//     return this.min.data;
+// }
+
+// function min_priority_queue_prnit() {
+//     var n = this.first;
+
+//     console.log('');
+//     console.log(n);
+//     for (n = n.next; n != null && n != this.first; n = n.next) {
+//         console.log(n);
+//     }
+//     console.log('');
+//     console.log('min:');
+//     console.log(this.min);
+//     console.log('================================================================================');
+// }
+
+// var pq = new_min_priority_queue();
+
+// pq.push('2_1', 2);
+// pq.push('6', 6);
+// pq.push('1', 1);
+// pq.push('2_2', 2);
+// pq.push('7', 7);
+// pq.push('42', 42);
+// pq.push('26', 26);
+// pq.push('3', 3);
+// pq.push('52', 52);
+// pq.print();
+
+// pq.pop_min();
+// pq.print();
