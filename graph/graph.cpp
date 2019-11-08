e0904bc2a140d61656a519995e5e4ce4e7c15735
Jonathan Tsai <jtsai@e1z2r3p37.42.us.org>
2019-11-08 12:57:36 -0800

    add

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -17,8 +17,8 @@ class graph
 	private:
 		g_node *g;
 		map<int, g_node *> id_map;
-		map<int, g_node *>::iterator it1;
-		map<int, g_node *>::iterator it2;
+		//map<int, g_node *>::iterator it1;
+		//map<int, g_node *>::iterator it2;
 
 	public:
 		graph()
@@ -37,13 +37,28 @@ class graph
 			return n;
 		}
 
+		g_node *add_graph_node(int id)
+		{
+			g_node *n;
+
+			auto it = id_map.find(id);
+			if (it != id_map.end())
+				return it->second;
+			n = new g_node;
+			n->id = id;
+			id_map.insert(pair<int, g_node *>(id, n));
+			if (!g)
+				g = n;
+			return n;
+		}
+
 		void add_edge(int a, int b)
 		{
 			g_node *n1;
 			g_node *n2;
 
-			it1 = id_map.find(a);
-			it2 = id_map.find(b);
+			auto it1 = id_map.find(a);
+			auto it2 = id_map.find(b);
 			n1 = (it1 == id_map.end()) ? new_graph_node(a) : it1->second;
 			n2 = (it1 == id_map.end()) ? new_graph_node(b) : it2->second;
 			if ((n1->nb).find(b) == n1->nb.end())
@@ -52,12 +67,40 @@ class graph
 				(n2->nb).insert(pair<int, g_node *>(a, n1));
 			}
 		}
+
+		void	print_graph()
+		{
+			g_node *n;
+			vector<int> v;
+
+			if (g == 0)
+			{
+				cout << "graph:\n(null)" << endl;
+				return ;
+			}
+			for (auto it = id_map.begin(); it != id_map.end(); it++)
+				v.push_back(it->first);
+			sort(v.begin(), v.end());
+			cout << "graph:" << endl;
+			for (unsigned i = 0; i < v.size(); i++)
+			{
+				cout << "node (" << v[i] << ") with nbrs:";
+				auto k = id_map.find(v[i]);
+				n = k->second;
+				for (auto it = n->nb.begin(); it != n->nb.end(); it++)
+					cout << " (" << it->first << ")";
+				cout << endl;
+			}
+		}
 };
 
 int main()
 {
 	graph g;
 
-	cout << "graph" << endl;
+	g.add_edge(3, 4);
+	g.add_edge(1, 2);
+	g.add_edge(1, 3);
+	g.print_graph();
 	return 0;
 }
